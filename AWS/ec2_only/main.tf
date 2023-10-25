8b48bf757e77e4a13e3d17583bd4cb7694ad2805
florina-alfred <sonofcop@gmail.com>
2023-10-25 10:47:40 +0200

    remade index.html

--- AWS/ec2_only/main.tf
+++ AWS/ec2_only/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Mom!" > index.html
+              curl https://raw.githubusercontent.com/Florina-Alfred/terraform/main/test.html > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
