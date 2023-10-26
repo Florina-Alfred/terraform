65c70f3b98ade90e36799056b538d47debcbed10
Florina-Alfred <108993117+Florina-Alfred@users.noreply.github.com>
2023-10-26 02:39:50 +0200

    reset

--- AWS/ags/main.tf
+++ AWS/ags/main.tf
@@ -37,6 +37,12 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_launch_configuration" "example" {
