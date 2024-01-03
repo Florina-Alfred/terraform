f46a4cf946d29b21ba596ac49c8ad3c2b3c03a20
florina-alfred <sonofcop@gmail.com>
2024-01-03 23:04:48 +0100

    Testing with Fugitive

--- AWS/ec2_cluster/main.tf
+++ AWS/ec2_cluster/main.tf
@@ -16,6 +16,12 @@ resource "aws_instance" "master_server" {
   ami                         = "ami-09e03e6bd1ff7ec01"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.instance.id]
+
+  network_interface {
+    network_interface_id = aws_network_interface.master_network_interface.id
+    device_index         = 0
+  }
+
   key_name                    = aws_key_pair.ssh_key.key_name
   user_data                   = <<-EOF
                                 #!/bin/bash
@@ -32,6 +38,10 @@ resource "aws_instance" "worker_1_server" {
   ami                         = "ami-09e03e6bd1ff7ec01"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.instance.id]
+  network_interface {
+    network_interface_id = aws_network_interface.worker_1_network_interface.id
+    device_index         = 0
+  }
   key_name                    = aws_key_pair.ssh_key.key_name
   user_data                   = <<-EOF
                                 #!/bin/bash
@@ -43,10 +53,15 @@ resource "aws_instance" "worker_1_server" {
     Name = var.worker_1_instance_name
   }
 }
+
 resource "aws_instance" "worker_2_server" {
   ami                         = "ami-09e03e6bd1ff7ec01"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.instance.id]
+  network_interface {
+    network_interface_id = aws_network_interface.worker_2_network_interface.id
+    device_index         = 0
+  }
   key_name                    = aws_key_pair.ssh_key.key_name
   user_data                   = <<-EOF
                                 #!/bin/bash
@@ -56,6 +71,51 @@ resource "aws_instance" "worker_2_server" {
   user_data_replace_on_change = true
   tags = {
     Name = var.worker_2_instance_name
+  }
+}
+
+resource "aws_vpc" "cluster_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-cluster-vpc"
+  }
+}
+
+resource "aws_subnet" "cluster_subnet" {
+  vpc_id            = aws_vpc.cluster_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-cluster-vpc"
+  }
+}
+
+resource "aws_network_interface" "master_network_interface" {
+  subnet_id   = aws_subnet.cluster_subnet.id
+  private_ips = ["172.16.10.101"]
+  security_groups = ["${aws_security_group.instance.id}"]
+  tags = {
+    Name = "terraform_primary_network_interface"
+  }
+}
+
+resource "aws_network_interface" "worker_1_network_interface" {
+  subnet_id   = aws_subnet.cluster_subnet.id
+  private_ips = ["172.16.10.102"]
+
+  tags = {
+    Name = "terraform_primary_network_interface"
+  }
+}
+
+resource "aws_network_interface" "worker_2_network_interface" {
+  subnet_id   = aws_subnet.cluster_subnet.id
+  private_ips = ["172.16.10.103"]
+
+  tags = {
+    Name = "terraform_primary_network_interface"
   }
 }
 
