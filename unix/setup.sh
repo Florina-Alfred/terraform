a4b9bd20ff97abc6e965394c615f907352ebbdbd
florina-alfred <sonofcop@gmail.com>
2024-01-07 15:11:19 +0100

    reinstalling argocd

--- unix/setup.sh
+++ unix/setup.sh
@@ -30,23 +30,23 @@ then
     # install k3s
     # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --write-kubeconfig-mode 644 --node-ip 10.0.10.101 --node-external-ip 10.0.10.101 --flannel-iface enX0 --token QnJpbmdpbmcgaW5kdXN0cmlhbCBzYWZldHkgYW5kIGF1dG9tYXRpb24gdG8gdGhlIGVkZ2Uu" sh -
     curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --write-kubeconfig-mode 644 --node-ip $private_ip --node-external-ip $private_ip --flannel-iface $network_interface --token QnJpbmdpbmcgaW5kdXN0cmlhbCBzYWZldHkgYW5kIGF1dG9tYXRpb24gdG8gdGhlIGVkZ2Uu" sh -
-    # sleep 3
+    sleep 3
     mkdir /home/ubuntu/.kube
     cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config
 
     # install helm
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-    # sleep 5
+    sleep 5
     helm repo add prometheus-repo https://prometheus-community.github.io/helm-charts
     helm repo update
     helm install monitoring prometheus-repo/kube-prometheus-stack
 
     # install argocd
-    # kubectl create namespace argocd
-    # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-    # curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-    # sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-    # rm argocd-linux-amd64
+    kubectl create namespace argocd
+    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+    rm argocd-linux-amd64
 
 elif [[ $(hostname) == master* ]]
 then
